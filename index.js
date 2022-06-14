58329349f3d0690b2f2f992c6c9846e3a4de1f43
allanstar-byte <allanmutisya263@gmail.com>
2022-06-14 05:13:55 +0300

    DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', e => {
+    console.log('After DOM loads')
+
+    let replace = document.querySelector("#text")
+    replace.innerHTML = "This is really cool!"
+        // console.log(replace)
+
+})
